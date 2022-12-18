8343287ed802aa105da094c0fc35f35f390ff3e7
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-18 10:44:53 +0700

    use memo

--- src/Memo/Index.js
+++ src/Memo/Index.js
@@ -1,9 +1,15 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import City from "./City";
+
+const kuadrat = (count) => {
+    console.log("kuadrat dijalankan");
+    return count * count;
+};
 
 function Index() {
     const [count, setCount] = useState(0);
     const [city, setCity] = useState(["Jakarta", "Kalimantan", "Surabaya"]);
+    let newKuadrat = useMemo(() => kuadrat(count), [count]);
 
     const addCity = useCallback(() => {
         setCity((prev) => [...prev, "Kota Baru"]);
@@ -12,6 +18,7 @@ function Index() {
     return (
         <div>
             <h1 style={{ textAlign: "center" }}>{count}</h1>
+            <h1 style={{ textAlign: "center" }}>{newKuadrat}</h1>
             <div style={{ textAlign: "center" }}>
                 <button onClick={() => setCount(count + 1)}>Increment</button>
             </div>
